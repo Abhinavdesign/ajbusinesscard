20c27ae30395d5e72345db7e34ca70ac26258277
Abhinav <abhinav.workmail.dn@gmail.com>
2024-07-17 13:34:04 +0530

    footerheightadjustment

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import Link from 'next/link';
 
 const Footer: React.FC = () => {
   return (
-    <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 flex justify-between items-center text-white" style={{ width: '60%' }}>
+    <div className="footer absolute bottom-10 left-1/2 transform -translate-x-1/2 flex justify-between items-center text-white" style={{ width: '60%' }}>
       <span className="text-sm font-serif">&copy; Abhinav Jha. All rights reserved.</span>
       <div className="flex space-x-4">
         <Link href="/terms-of-use" legacyBehavior>
