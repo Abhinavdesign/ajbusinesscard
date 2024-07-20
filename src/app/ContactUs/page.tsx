d6d21f950daf8b9732e49f143eb7fa379ca78590
Abhinav <abhinav.workmail.dn@gmail.com>
2024-07-20 08:46:28 +0530

    finalchanges

--- src/app/ContactUs/page.tsx
+++ src/app/ContactUs/page.tsx
@@ -1,42 +1,15 @@
 'use client';
 
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
 import FooterC from '../components/FooterC'; // Import the new FooterC component
 import DownloadModal from '../components/DownloadModal'; // Import the DownloadModal component
 import Image from 'next/image'; // Import Image from next/image
-import Preloader from '../components/Preloader'; // Import the Preloader component
-import './contactus.css'; // Import the scoped CSS
+import './contactus.css';// Import the scoped CSS
 
 const ContactUs: React.FC = () => {
-  const [loading, setLoading] = useState(true);
-  const [showDownloadModal, setShowDownloadModal] = useState(false); // Define showDownloadModal state
-  const imagesToLoad = useRef(1); // Start with 1 to account for initial state
-
-  const handleImageLoad = () => {
-    imagesToLoad.current -= 1;
-    if (imagesToLoad.current === 0) {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    const handleLoad = () => {
-      console.log('Page fully loaded');
-      setLoading(false);
-    };
-
-    if (document.readyState === 'complete') {
-      handleLoad();
-    } else {
-      window.addEventListener('load', handleLoad);
-
-      return () => {
-        window.removeEventListener('load', handleLoad);
-      };
-    }
-  }, []);
+  const [showDownloadModal, setShowDownloadModal] = useState(false);
 
   useEffect(() => {
     // Adjust styles if necessary on initial render and resize
@@ -99,8 +72,8 @@ const ContactUs: React.FC = () => {
 
   const handleDownload = () => {
     const link = document.createElement('a');
-    link.href = '/test.pdf'; // Adjust the path according to your setup
-    link.download = 'test.pdf';
+    link.href = '/Brouchure.pdf'; // Adjust the path according to your setup
+    link.download = 'Brouchure.pdf';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -108,106 +81,95 @@ const ContactUs: React.FC = () => {
   };
 
   return (
-    <>
-      {loading && <Preloader />}
-      <motion.div
-        initial={{ opacity: 0, x: '100vw' }}
-        animate={{ opacity: 1, x: 0 }}
-        exit={{ opacity: 0, x: '-100vw' }}
-        transition={{ duration: 0.5 }}
-        className="ContactUs relative flex flex-col justify-center items-center min-h-screen overflow-hidden"
-        style={{ display: loading ? 'none' : 'block' }}
+    <motion.div
+      initial={{ opacity: 0, x: '100vw' }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: '-100vw' }}
+      transition={{ duration: 0.5 }}
+      className="ContactUs relative flex flex-col justify-center items-center min-h-screen overflow-hidden"
+    >
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover"
+        autoPlay
+        loop
+        muted
       >
-        <video
-          className="absolute top-0 left-0 w-full h-full object-cover"
-          autoPlay
-          loop
-          muted
-        >
-          <source src="/cherryvideo.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-        <div className="container">
-          <div className="icon-group">
-            <Image
-              src="/Phoneicon.png"
-              alt="Phone Icon"
-              className="icon"
-              width={80}
-              height={80}
-              onLoad={handleImageLoad}
-            />
-            <Image
-              src="/Phoneno.png"
-              alt="Phone Number"
-              className="text"
-              width={700}
-              height={150}
-              onLoad={handleImageLoad}
-            />
-          </div>
-          <div className="icon-group">
-            <Image
-              src="/emailicon.png"
-              alt="Email Icon"
-              className="icon"
-              width={80}
-              height={80}
-              onLoad={handleImageLoad}
-            />
-            <Image
-              src="/emailid.png"
-              alt="Email ID"
-              className="text"
-              width={700}
-              height={150}
-              onLoad={handleImageLoad}
-            />
-          </div>
+        <source src="/cherryvideo.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="container">
+        <div className="icon-group">
+          <Image
+            src="/Phoneicon.png"
+            alt="Phone Icon"
+            className="icon"
+            width={80}
+            height={80}
+          />
+          <Image
+            src="/Phoneno.png"
+            alt="Phone Number"
+            className="text"
+            width={700}
+            height={150}
+          />
         </div>
-        <div className="top-icons">
-          <button onClick={handleDownloadClick} className="top-icon">
+        <div className="icon-group">
+          <Image
+            src="/emailicon.png"
+            alt="Email Icon"
+            className="icon"
+            width={80}
+            height={80}
+          />
+          <Image
+            src="/emailid.png"
+            alt="Email ID"
+            className="text"
+            width={700}
+            height={150}
+          />
+        </div>
+      </div>
+      <div className="top-icons">
+        <button onClick={handleDownloadClick} className="top-icon">
+          <Image
+            src="/download.png"
+            alt="Download Icon"
+            width={30}
+            height={30}
+          />
+        </button>
+        <Link href="/project" legacyBehavior>
+          <a>
             <Image
-              src="/download.png"
-              alt="Download Icon"
+              src="/projecticon.png"
+              alt="Project Icon"
+              className="top-icon"
               width={30}
               height={30}
-              onLoad={handleImageLoad}
             />
-          </button>
-          <Link href="/project" legacyBehavior>
-            <a>
-              <Image
-                src="/projecticon.png"
-                alt="Project Icon"
-                className="top-icon"
-                width={30}
-                height={30}
-                onLoad={handleImageLoad}
-              />
-            </a>
-          </Link>
-          <Link href="/" legacyBehavior>
-            <a>
-              <Image
-                src="/home.png"
-                alt="Home Icon"
-                className="top-icon"
-                width={30}
-                height={30}
-                onLoad={handleImageLoad}
-              />
-            </a>
-          </Link>
-        </div>
-        <div className="footer-container">
-          <FooterC />
-        </div>
-        {showDownloadModal && (
-          <DownloadModal onClose={handleCloseModal} onDownload={handleDownload} />
-        )}
-      </motion.div>
-    </>
+          </a>
+        </Link>
+        <Link href="/" legacyBehavior>
+          <a>
+            <Image
+              src="/home.png"
+              alt="Home Icon"
+              className="top-icon"
+              width={30}
+              height={30}
+            />
+          </a>
+        </Link>
+      </div>
+      <div className="footer-container">
+        <FooterC />
+      </div>
+      {showDownloadModal && (
+        <DownloadModal onClose={handleCloseModal} onDownload={handleDownload} />
+      )}
+    </motion.div>
   );
 };
 
