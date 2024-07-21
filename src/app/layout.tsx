e77052c642998b775ee2ded7995720dd30e533dd
Abhinav <abhinav.workmail.dn@gmail.com>
2024-07-21 16:08:06 +0530

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
+import React from 'react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import PageTransition from './components/PageTransition'; // Adjusted the path
+import { Analytics } from "@vercel/analytics/react"; // Importing Vercel Analytics
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,22 +18,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>{String(metadata.title) || 'Default Title'}</title>
-        <meta name="description" content={String(metadata.description) || 'Default Description'} />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
-        <link rel="icon" href="/favicon.ico" />
-      </head>
-      <body className={inter.className}>
-        <PageTransition>
-          {children}
-        </PageTransition>
-      </body>
-    </html>
+    <React.Fragment>
+      <html lang="en">
+        <head>
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>{String(metadata.title) || 'Default Title'}</title>
+          <meta name="description" content={String(metadata.description) || 'Default Description'} />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
+          <link rel="icon" href="/favicon.ico" />
+        </head>
+        <body className={inter.className}>
+          <PageTransition>
+            {children}
+          </PageTransition>
+          <Analytics /> {/* Adding Vercel Analytics */}
+        </body>
+      </html>
+    </React.Fragment>
   );
 }
